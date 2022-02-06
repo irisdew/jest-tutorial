00559ae3cad0e54cecf843bc8d623e58416cad3d
irisdew <ycleardew@gmail.com>
2022-02-06 14:33:03 +0900

    feat: toBe, toEqaul, toStrictEqual

--- fn.js
+++ fn.js
@@ -1,5 +1,10 @@
 const fn = {
   add: (num1, num2) => num1 + num2,
+  makeUser: (name, age) => ({
+    name,
+    age,
+    gender: undefined,
+  }),
 };
 
 module.exports = fn;
--- fn.test.js
+++ fn.test.js
@@ -1,3 +1,4 @@
+const { expect } = require("@jest/globals");
 const fn = require("./fn.js");
 
 test("1은 1이다", () => {
@@ -8,6 +9,22 @@ test("2 더하기 3은 5다", () => {
   expect(fn.add(2, 3)).toBe(5);
 });
 
+test("2 더하기 3은 5야", () => {
+  expect(fn.add(2, 3)).toEqual(5);
+});
+
 test("3 더하기 3은 5가 아니다", () => {
   expect(fn.add(3, 3)).not.toBe(5);
+});
+
+test("이름, 나이를 입력하면 객체를 반환해줘 - toBe", () => {
+  expect(fn.makeUser("Mike", 30)).toBe({ name: "Mike", age: 30 });
+});
+
+test("이름, 나이를 입력하면 객체를 반환해줘 - toStrictEqaul", () => {
+  expect(fn.makeUser("Mike", 30)).toStrictEqual({ name: "Mike", age: 30 });
+});
+
+test("이름, 나이를 입력하면 객체를 반환해줘 - toEqaul", () => {
+  expect(fn.makeUser("Mike", 30)).toEqual({ name: "Mike", age: 30 });
 });
