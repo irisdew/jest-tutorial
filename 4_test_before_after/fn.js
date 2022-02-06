a96f370e28f9cdea08b19883ec7a853986af306d
irisdew <ycleardew@gmail.com>
2022-02-06 23:12:33 +0900

    feat: beforeAll, afterAll

--- 4_test_before_after/fn.js
+++ 4_test_before_after/fn.js
@@ -1,5 +1,25 @@
 const fn = {
   add: (num1, num2) => num1 + num2,
+  connectUserDB: () => {
+    return new Promise((res) => {
+      setTimeout(() => {
+        res({
+          name: "Mike",
+          age: 30,
+          gender: "male",
+        });
+      }, 500);
+    });
+  },
+  disconnectDB: () => {
+    return new Promise((res) => {
+      setTimeout(() => {
+        setTimeout(() => {
+          res();
+        });
+      }, 500);
+    });
+  },
 };
 
 module.exports = fn;
--- 4_test_before_after/fn.test.js
+++ 4_test_before_after/fn.test.js
@@ -1,23 +1,18 @@
 const fn = require("./fn");
 
-let num = 10;
+let user;
 
-// 각 테스트 직전에 실행
-afterEach(() => {
-  num = 0;
+// 각 테스트마다 실행되지 않고 모든 테스트 진행 전/후로 1번만 실행
+beforeAll(async () => {
+  user = await fn.connectUserDB();
+});
+afterAll(() => {
+  return fn.disconnectDB();
 });
 
-test("0 더하기 1은 1이다.", () => {
-  num = fn.add(num, 1);
-  expect(num).toBe(1);
+test("유저 이름은 Mike", () => {
+  expect(user.name).toBe("Mike");
 });
-
-test("0 더하기 2은 2이다.", () => {
-  num = fn.add(num, 2);
-  expect(num).toBe(2);
-});
-
-test("0 더하기 3은 3이다.", () => {
-  num = fn.add(num, 3);
-  expect(num).toBe(3);
+test("유저 나이는 30", () => {
+  expect(user.age).toBe(30);
 });
