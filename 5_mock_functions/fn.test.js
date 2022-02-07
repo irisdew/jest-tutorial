a2a9abd8edfe6efc8d753313dac52ade77358a1f
irisdew <ycleardew@gmail.com>
2022-02-07 23:26:57 +0900

    feat: mock function test

--- 5_mock_functions/fn.test.js
+++ 5_mock_functions/fn.test.js
@@ -5,7 +5,10 @@ const mockFn = jest.fn();
 mockFn();
 mockFn(1);
 
-test("dd", () => {
-  console.log(mockFn.mock.calls); // [ [], [ 1 ] ]
-  expect("dd").toBe("dd");
+test("함수는 2번 호출됩니다.", () => {
+  expect(mockFn.mock.calls.length).toBe(2);
+});
+
+test("2번째로 호출된 함수에 전달된 첫번째 인수는 1 입니다.", () => {
+  expect(mockFn.mock.calls[1][0]).toBe(1);
 });
