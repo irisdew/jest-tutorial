485ee89023070a4a2252924ca344cefc147021ab
irisdew <ycleardew@gmail.com>
2022-02-08 00:03:04 +0900

    feat: test with mocking module

--- 5_mock_functions/fn.test.js
+++ 5_mock_functions/fn.test.js
@@ -1,7 +1,13 @@
 const fn = require("./fn");
 
-// test는 통과하지만 실제로 유저가 생성되어버림!
+jest.mock("./fn");
+// jest.mock으로 fn 모듈을 mocking 모듈로 만들어줌
+
+fn.createUser.mockReturnValue({ name: "Monika" });
+
 test("유저를 만든다", () => {
   const user = fn.createUser("Monika");
   expect(user.name).toBe("Monika");
 });
+// test 통과
+// 실제로 유저가 생성되지 않음 = 실제 createUser 함수가 실행되지 않음
