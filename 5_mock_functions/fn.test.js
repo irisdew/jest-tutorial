b6acb006a125319b4a13155aa398a89f917a25ab
irisdew <ycleardew@gmail.com>
2022-02-07 23:35:19 +0900

    feat: mock.results

--- 5_mock_functions/fn.test.js
+++ 5_mock_functions/fn.test.js
@@ -1,21 +1,20 @@
 // mock function
 
-const mockFn = jest.fn();
+const mockFn = jest.fn((num) => num + 1);
+// jest.fn 의 첫번째 인수로 함수를 전달
 
-function forEachAdd1(arr) {
-  arr.forEach((num) => {
-    // 여기에 들어갈 임의의 함수를 작성하지 말고, 빠르고 간단하게 mock 함수 만들기
-    mockFn(num + 1);
-  });
-}
-
-forEachAdd1([10, 20, 30]);
+mockFn(10);
+mockFn(20);
+mockFn(30);
 
 test("함수 호출은 3번 됩니다.", () => {
+  console.log(mockFn.mock.results);
+  /*
+  [
+    { type: "return", value: 11 },
+    { type: "return", value: 21 },
+    { type: "return", value: 31 },
+  ];
+  */
   expect(mockFn.mock.calls.length).toBe(3);
-});
-test("함수의 return으로 전달된 값은 11, 21, 31입니다.", () => {
-  expect(mockFn.mock.calls[0][0]).toBe(11);
-  expect(mockFn.mock.calls[1][0]).toBe(21);
-  expect(mockFn.mock.calls[2][0]).toBe(31);
 });
