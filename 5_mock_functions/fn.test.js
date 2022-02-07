797775b88de8e89cbc101de19d08a00c68ed8016
irisdew <ycleardew@gmail.com>
2022-02-07 23:38:12 +0900

    feat: test using mock.results

--- 5_mock_functions/fn.test.js
+++ 5_mock_functions/fn.test.js
@@ -1,5 +1,7 @@
 // mock function
 
+const { expect } = require("@jest/globals");
+
 const mockFn = jest.fn((num) => num + 1);
 // jest.fn 의 첫번째 인수로 함수를 전달
 
@@ -7,14 +9,12 @@ mockFn(10);
 mockFn(20);
 mockFn(30);
 
-test("함수 호출은 3번 됩니다.", () => {
-  console.log(mockFn.mock.results);
-  /*
-  [
-    { type: "return", value: 11 },
-    { type: "return", value: 21 },
-    { type: "return", value: 31 },
-  ];
-  */
-  expect(mockFn.mock.calls.length).toBe(3);
+test("10에서 1 증가한 값이 반환된다.", () => {
+  expect(mockFn.mock.results[0].value).toBe(11);
+});
+test("20에서 1 증가한 값이 반환된다.", () => {
+  expect(mockFn.mock.results[1].value).toBe(21);
+});
+test("30에서 1 증가한 값이 반환된다.", () => {
+  expect(mockFn.mock.results[2].value).toBe(31);
 });
