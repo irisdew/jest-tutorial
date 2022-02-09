aa5af10b883aaf99e69bdc68bc437415a407b9ba
irisdew <ycleardew@gmail.com>
2022-02-10 00:00:30 +0900

    feat: add snapshot test

--- 6_snapshot_test/src/component/Hello.test.js
+++ 6_snapshot_test/src/component/Hello.test.js
@@ -2,7 +2,11 @@ import { render, screen } from "@testing-library/react";
 import Hello from "./Hello";
 
 const user = {
-  name: "Mike",
+  name: "Tom",
+  age: 20,
+};
+
+const userWithoutName = {
   age: 20,
 };
 
@@ -10,4 +14,14 @@ test("Hello라는 글자가 포함?", () => {
   render(<Hello user={user} />);
   const helloEl = screen.getByText(/Hello/i);
   expect(helloEl).toBeInTheDocument();
+});
+
+test("snapshot: name 있음", () => {
+  const el = render(<Hello user={user} />);
+  expect(el).toMatchSnapshot();
+});
+
+test("snapshot: name 없음", () => {
+  const el = render(<Hello user={userWithoutName} />);
+  expect(el).toMatchSnapshot();
 });
