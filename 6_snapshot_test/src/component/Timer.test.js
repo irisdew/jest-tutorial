a464bb7309aac1a8e3adaff23eae163e849ca4f1
irisdew <ycleardew@gmail.com>
2022-02-10 00:23:35 +0900

    fix: add mock function and test paseed

--- 6_snapshot_test/src/component/Timer.test.js
+++ 6_snapshot_test/src/component/Timer.test.js
@@ -2,6 +2,9 @@ import { render } from "@testing-library/react";
 import Timer from "./Timer";
 
 test("초 표시", () => {
+  Date.now = jest.fn(() => 11212342);
+  // 고정된 숫자 반환하는 mockfunction
+  // Date.now는 이제 고정값
   const el = render(<Timer />);
   expect(el).toMatchSnapshot();
 });
